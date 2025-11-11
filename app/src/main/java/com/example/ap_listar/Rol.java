ba75b91a8caf7dbbb830d952c5e8d7d336eb24ad
Didier Flores <saucedofloresdidier@gmail.com>
2025-11-11 10:54:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ap_listar/Rol.java
@@ -0,0 +1,49 @@
+package com.example.ap_listar_11_11_2025;
+
+public class Rol {
+    private int id;
+    private String nombre;
+    private String descripcion;
+
+    public Rol(){}
+
+    public Rol(int id,String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+    public Rol(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - " + descripcion;
+    }
+}
